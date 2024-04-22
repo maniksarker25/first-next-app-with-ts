45100fa74ee1a3811a9b2be74ee9f414c18c4852
Manik <tonysins43@gmail.com>
2024-04-22 23:42:50 +0600

    show blog details page

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,10 +1,22 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types";
 
 interface Params {
   blogId: string;
 }
+
+export const generateStaticParams = async () => {
+  const res = await fetch(`http://localhost:5000/blogs`);
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
 const BlogDetailPage = async ({ params }: { params: Params }) => {
-  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`);
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
   const blog = await res.json();
 
   return (
